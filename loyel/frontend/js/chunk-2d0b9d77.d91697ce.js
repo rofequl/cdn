63b6f7eacc068bfb8ca7f31a80449b870de592e8
rofequl <rofequlislamnayem@gmail.com>
2021-03-16 01:57:03 +0600

    first commit

---
+++ loyel/frontend/js/chunk-2d0b9d77.d91697ce.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0b9d77"],{3554:function(e,n,t){"use strict";t.r(n);var c=function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("div")},u=[],r={name:"ProductList"},s=r,a=t("2877"),i=Object(a["a"])(s,c,u,!1,null,"5ffce877",null);n["default"]=i.exports}}]);
+//# sourceMappingURL=chunk-2d0b9d77.d91697ce.js.map
