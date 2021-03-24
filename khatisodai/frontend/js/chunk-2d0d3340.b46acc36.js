b5a87948b1fa86cf6404789d98a4f75ee95fe741
rofequl <rofequlislamnayem@gmail.com>
2021-03-24 15:04:39 +0600

    first commit

---
+++ khatisodai/frontend/js/chunk-2d0d3340.b46acc36.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0d3340"],{"5c84":function(n,w){}}]);
+//# sourceMappingURL=chunk-2d0d3340.b46acc36.js.map
---
+++ khatisodai/frontend/js/chunk-2d21afc5.aca2c3be.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d21afc5"],{be9e:function(n,c,e){"use strict";e.r(c),c["default"]={}}}]);
+//# sourceMappingURL=chunk-2d21afc5.aca2c3be.js.map
